39f1346aa9268ffbae62640d7828c8fc3638c72c
Vitor Conceicao <vitor_rc@outlook.com>
2026-02-08 08:53:10 -0300

    style: remove lines

--- Projects/App/Sources/Application/SceneDelegate.swift
+++ Projects/App/Sources/Application/SceneDelegate.swift
@@ -14,9 +14,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene,
                willConnectTo session: UISceneSession,
                options connectionOptions: UIScene.ConnectionOptions) {
-        guard let windowScene = (scene as? UIWindowScene) else {
-            return
-        }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
 
         let window = UIWindow(windowScene: windowScene)
         let nav = UINavigationController(rootViewController: ViewController())
