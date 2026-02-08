abd84c4720196165f82d2abc8e03ae7293d8a796
Vitor Conceicao <vitor_rc@outlook.com>
2026-02-08 16:26:00 -0300

    tests: add dependency injector spy

--- Projects/DependencyInjection/Project.swift
+++ Projects/DependencyInjection/Project.swift
@@ -8,5 +8,5 @@ let dependecies: [TargetDependency] = [
 ]
 
 let project = Project.templateModule(named: moduleName,
-                                     targets: [.source,. interfaces, .test],
+                                     targets: [.source,. interfaces, .test, .testing],
                                      dependencies: dependecies)
