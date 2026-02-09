699173d84b45660d958a14f5a151a9ab615928bc
Vitor Rodrigues <42953452+vitor-rc1@users.noreply.github.com>
2026-02-09 11:59:06 -0300

    tests: add home coordinator tests (#26)

--- Projects/DependencyInjection/Testing/DependencyInjectorSpy.swift
+++ Projects/DependencyInjection/Testing/DependencyInjectorSpy.swift
@@ -12,8 +12,6 @@ public final class DependencyInjectorSpy: DependencyInjector {
     public init() {}
 
     public enum Method {
-        case register
-        case registerWithArgument
         case resolve
         case resolveWithArgument
     }
@@ -30,14 +28,12 @@ public final class DependencyInjectorSpy: DependencyInjector {
 
     public func register<Service>(_ serviceType: Service.Type,
                                   factory: @escaping (DependencyResolver) -> Service) {
-        calledMethods.append(.register)
         let identifier = typeIdentifier(serviceType)
         registeredServices[identifier] = factory
     }
 
     public func register<Service, Argument>(_ serviceType: Service.Type,
                                             factory: @escaping (DependencyResolver, Argument) -> Service) {
-        calledMethods.append(.registerWithArgument)
         let identifier = typeIdentifier(serviceType)
         registeredServicesWithArgument[identifier] = factory
     }
